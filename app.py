71f39be305677422239e2d10851847ab1ee51c52
Govinda <gkhabas76@gmail.com>
2020-09-28 00:12:31 +0200

    added: voice automation for opening programs from computer and play songs from youtube

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import playsound
 
 
 def Daisy(text):
-    print(text)
+    # print(text)
     tts = gTTS(text=text, lang='en-uk')
     soundSource = 'audio.mp3'
     tts.save(soundSource)
@@ -31,16 +31,35 @@ def myWords():
     return myInputAudio
 
 
+def playSong():
+    url = 'https://www.youtube.com/watch?v=2Vv-BfVoq4g'
+    browserPath = r'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
+    webbrowser.register(
+        'chrome', None, webbrowser.BackgroundBrowser(browserPath))
+    webbrowser.get('chrome').open_new_tab(url)
+
+
 Daisy('Welcome to- Digital Assistant as Integrated Speech Yonder. By the way, you can call me Daisy.')
 
 # Todo --> Login verification function
+# Todo from file. Store some text as password for Daisy to read.
 #Daisy('Please say the password')
 
 
 def commands(command):
     if 'hello' in command:
         Daisy('Hi, Govinda. How are you?')
-    elif 'stop the program' in command:
+
+    elif 'could you please play any songs of your choice' in command:
+        Daisy('Cool. I will play the song, titled, Perfect by Ed Sheeran')
+        return playSong()
+
+    elif 'open pycharm' in command:
+        Daisy('Opening PyCharm. Please wait.')
+        os.startfile(
+            r"C:\Program Files\JetBrains\PyCharm Community Edition 2019.1.1\bin\pycharm64.exe")
+
+    elif 'stop the program please' in command:
         Daisy('Shutting down the program')
         quit()
 
